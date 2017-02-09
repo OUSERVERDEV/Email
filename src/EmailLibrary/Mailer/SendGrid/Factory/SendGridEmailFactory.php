699fd500e3069f12cfbb3a0adc16faaa0ba57b4b
Rabie SULMAN <rabie@iron-mail.co.uk>
2017-02-09 15:15:39 +0000

    remove commented fun

--- src/EmailLibrary/Mailer/SendGrid/Factory/SendGridEmailFactory.php
+++ src/EmailLibrary/Mailer/SendGrid/Factory/SendGridEmailFactory.php
@@ -12,35 +12,6 @@ use SendGrid\Personalization;
 
 class SendGridEmailFactory implements SendGridEmailFactoryInterface
 {
-//    /**
-//     * {@inheritDoc}
-//     */
-//    public function createSendGridEmail(EmailInterface $email)
-//    {
-//        $sendGridEmail = new SendGridEmail();
-//
-//        $sendGridEmail->to = $email->getTo();
-//        $sendGridEmail->setFrom($email->getFrom());
-//        $sendGridEmail->setFromName($email->getFromName());
-//        $sendGridEmail->setSubject($email->getSubject());
-//        $sendGridEmail->setText($email->getBodyText());
-//        $sendGridEmail->setHtml($email->getBodyHtml());
-//
-//        if (is_array($email->getAttachments())) {
-//            $sendGridEmail->setAttachments($email->getAttachments());
-//        }
-//
-//        if ($email instanceof EmailDecorator) {
-//            $sendGridEmail->setCategories($email->getCategories());
-//
-//            if (!is_null($email->getSendAt())) {
-//                $sendGridEmail->setSendAt($email->getSendAt());
-//            }
-//        }
-//
-//        return $sendGridEmail;
-//    }
-
     /**
      * {@inheritDoc}
      */
